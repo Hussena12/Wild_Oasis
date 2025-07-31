6b20e2e3a481ccac7a1f18a5c246cfe3d2b16a6a
Hussen <ha0959943@gmail.com>
2025-07-31 20:39:37 +0300

    initial setup

---
+++ app/account/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const page = () => {
+  return (
+    <div>
+      <Navigation />
+      account page
+    </div>
+  );
+};
+
+export default page;
---
+++ app/cabins/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const page = () => {
+  return (
+    <div>
+      <Navigation />
+      cabins page
+    </div>
+  );
+};
+
+export default page;
