de22391971fd68682fbfd30285903c28d7eb0db0
Hussen <ha0959943@gmail.com>
2025-08-05 07:54:24 +0300

    home page finished

--- app/page.js
+++ app/page.js
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import hero from "@/public/bg.png";
+
 export default function Home() {
-  return <h1>welcome to ur home</h1>;
+  return (
+    <main>
+      <Image
+        src={hero}
+        placeholder="blur"
+        priority
+        className="object-cover object-top"
+        fill
+        alt="Mountains and forests with two cabins"
+      />
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
+  );
 }
