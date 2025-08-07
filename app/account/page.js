e720358a114830a11d99d18ef37a82cfec37f5ef
Hussen <ha0959943@gmail.com>
2025-08-07 22:08:41 +0300

    avatar page created

--- app/account/page.js
+++ app/account/page.js
@@ -10,6 +10,9 @@ const page = () => {
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
         Welcome, Jonas
       </h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome, Jonas
+      </h2>
     </div>
   );
 };
