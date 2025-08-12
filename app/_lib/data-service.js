0cabc4d31a650b40ed0ab877b58288c66def25de
Hussen <ha0959943@gmail.com>
2025-08-12 09:31:30 +0300

    loading implementation

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -10,9 +10,6 @@ export async function getCabin(id) {
     .eq("id", id)
     .single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
-
   if (error) {
     console.error(error);
   }
@@ -40,6 +37,8 @@ export const getCabins = async function () {
     .select("id, name, maxCapacity, regularPrice, discount, image")
     .order("name");
 
+  // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
